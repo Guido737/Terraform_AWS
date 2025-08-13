42c94e6af84dd2c14d244f8068a7cd40ea2de8e0
usernamezero00 <marselsavdalyan95@inbox.ru>
2025-08-13 18:46:53 +0400

    testing vpc's

---
+++ Lesson-30/modules/aws_network/main.tf
@@ -0,0 +1,24 @@
+#----------------------------------------------------------
+# My Terraform
+# Provision:
+#  - VPC
+#  - Internet Gateway
+
+# Made By Creator Eversor 13/08/25
+#----------------------------------------------------------
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
